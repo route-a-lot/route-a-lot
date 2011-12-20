693ab80de356c9a94c044e49b9a8f28d501234d5
Daniel Krauß <danielrkrauss@online.de>
2011-12-20 19:28:04 +0000

    KOnstruktor, sowie getter und setter erstellt

--- src/kit/route/a/lot/map/POINode.java
+++ src/kit/route/a/lot/map/POINode.java
@@ -1,5 +1,6 @@
 package kit.route.a.lot.map;
 
+import kit.route.a.lot.common.Coordinates;
 import kit.route.a.lot.common.POIDescription;
 
 public class POINode extends Node {
@@ -9,4 +10,13 @@ public class POINode extends Node {
      * 
      */
     private POIDescription info;
+
+    public POINode(int id, Coordinates position, POIDescription description){
+        super(id, position);
+        this.info = description;
+    }
+    
+    public POIDescription getInfo() {
+        return info;
+    }
 }
