d1f1cc723a581858ab6af56585fc7318c14dc315
Jan Jacob <jjanjjacob@googlemail.com>
2011-12-20 21:04:52 +0000

    Sorry wrong dir, forgot IntTouple

---
+++ src/kit/route/a/lot/common/IntTouple.java
@@ -0,0 +1,18 @@
+public Class IntTouple {
+    // It's a real pain to programm w/o one, consder it a Flyweight or something.
+    int firat;
+    int last;
+
+    public IntTouple(int first; int last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getLast() {
+        return last;
+    }
+}
