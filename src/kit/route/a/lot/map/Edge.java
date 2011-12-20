3709107aa4004fc05946cb52d8b8635567f6ffdb
Daniel Krauß <danielrkrauss@online.de>
2011-12-20 19:21:09 +0000

    Attribute für den Start und End-punkt, sowie Konstruktor und Getter hinzugefügt

--- src/kit/route/a/lot/map/Edge.java
+++ src/kit/route/a/lot/map/Edge.java
@@ -8,6 +8,25 @@ import kit.route.a.lot.common.Selection;
 
 
 public class Edge extends MapElement {
+    
+    /**
+     * Constructor
+     */
+    private Node start;
+    private Node end;
+    
+    public Edge(Node start, Node end){
+        this.start = start;
+        this.end = end;
+    }
+    
+    public Node getStart() {
+        return start;
+    }
+
+    public Node getEnd() {
+        return end;
+    }
 
     @Override
     protected String getName() {
