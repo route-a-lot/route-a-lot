c9504026b58da1c13e681f65e54b5d32fab3517b
Daniel Krauß <danielrkrauss@online.de>
2011-12-22 15:37:26 +0000

    Event für die Änderung der Sicht hinzugefügt

---
+++ src/kit/route/a/lot/gui/ViewChangedEvent.java
@@ -0,0 +1,48 @@
+package kit.route.a.lot.gui;
+
+
+public class ViewChangedEvent extends java.util.EventObject {
+    
+    /*describes the horizantal movement of an mapObject 
+     *in a number between -1 (western movement) and one 1 (eastern movement)
+     */ 
+    private float longitude;
+    
+    /*
+     * describes the vertical movement of an mapObject 
+     * in a number between -1 (norther movement) and one 1 (south movement)
+    */ 
+    private float laltitude;
+    
+    /*
+     * describes the factor, the zoomlevel was changed:
+     * -1: zoom out
+     * 0: no change
+     * 1: zoom in
+     */
+    private int zoomLevelChange;
+   
+    public ViewChangedEvent(Object source, float latitude, float longitude, int zoomLevelChange) {
+        super(source);
+        this.laltitude = latitude;
+        this.longitude = longitude;
+        this.zoomLevelChange = zoomLevelChange;
+    }
+
+    
+    
+    public int getZoomLevelChange() {
+        return zoomLevelChange;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public float getLaltitude() {
+        return laltitude;
+    }
+
+    
+    
+}
