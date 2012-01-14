65b7edb62bbf65b3cd0ca6c6fe427b1ebea16dee
Jan Jacob <jjanjjacob@googlemail.com>
2012-01-14 16:57:38 +0000

    Fixed errors preventing it from compiling

--- java/src/kit/route/a/lot/common/WeightCalculator.java
+++ java/src/kit/route/a/lot/common/WeightCalculator.java
@@ -26,24 +26,24 @@ public class WeightCalculator {
      * @return int
      */
     public int calcWeightWithHeight(int fromID, int toID) {
-        Coordinates from = State.getInstance().loadedMapInfo.getNodePosition(fromID);
-        Coordinates to = State.getInstance().loadedMapInfo.getNodePosition(toID);
+        Coordinates from = State.getInstance().getLoadedMapInfo().getNodePosition(fromID);
+        Coordinates to = State.getInstance().getLoadedMapInfo().getNodePosition(toID);
         Heightmap heightmap = State.getInstance().getHeightMap();
         // Pythagoras.
         return (int) Math.sqrt(
                     Math.pow((from.getLatitude() - to.getLatitude()), 2) + 
                     Math.pow((from.getLongitude() - to.getLongitude()), 2) + 
-                    Math.pow((State.getHeightMalus() * (heightmap.getHeight(from) - heightmap.getHeight(to))), 2));
+                    Math.pow((State.getInstance().getHeightMalus() * (heightmap.getHeight(from) - heightmap.getHeight(to))), 2));
     }
 
 
 
     public int calcWeight(int fromID, int toID) {
-        Coordinates from = State.getInstance().loadedMapInfo.getNodePosition(fromID);
-        Coordinates to = State.getInstance().loadedMapInfo.getNodePosition(toID);
+        Coordinates from = State.getInstance().getLoadedMapInfo().getNodePosition(fromID);
+        Coordinates to = State.getInstance().getLoadedMapInfo().getNodePosition(toID);
         
         //Kugelkoordinaten berechnet distance in km
-        return (int) = 6378.388 * Math.acos(Math.sin(from.getLatitude()) * Math.sin(to.getLatitude()) + Math.cos(from.getLatitude()) * Math.cos(to.getLatitude()) * Math.cos(to.getLongitude() - from.getLongitude()));
+        return (int) (6378.388 * Math.acos(Math.sin(from.getLatitude()) * Math.sin(to.getLatitude()) + Math.cos(from.getLatitude()) * Math.cos(to.getLatitude()) * Math.cos(to.getLongitude() - from.getLongitude())));
     }
     
     public int calcWeight(Selection edge) {
