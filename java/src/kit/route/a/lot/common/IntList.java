e04f8d9bb13eb6b0e2dd34975f691f81cd645162
Josua Stabenow <josua.stabenow@gmx.de>
2012-01-15 15:57:54 +0000

    IntList added (currently not used). GUI Fix pt2.

---
+++ java/src/kit/route/a/lot/common/IntList.java
@@ -0,0 +1,31 @@
+package kit.route.a.lot.common;
+
+
+public class IntList {
+    protected int[] data = new int[8];
+    protected transient int size = 0;
+
+    /** Return an element of the array */
+    public int get(int index) throws ArrayIndexOutOfBoundsException {
+      if (index >= size) {
+        throw new ArrayIndexOutOfBoundsException(index);
+      } else {
+        return data[index];
+      }
+    }
+
+    /** Add an int to the array, growing the array if necessary */
+    public void add(int x) {
+      if (data.length == size) {
+        resize(data.length * 2);
+      }
+      data[size++] = x;
+    }
+
+    /** An internal method to change the allocated size of the array */
+    protected void resize(int newsize) {
+      int[] newdata = new int[newsize];
+      System.arraycopy(data, 0, newdata, 0, size); 
+      data = newdata;
+    }
+}
---
+++ test/src/kit/route/a/lot/gui/ShellGUI.java
@@ -0,0 +1,19 @@
+package kit.route.a.lot.gui;
+
+import kit.route.a.lot.gui.GUI;
+import kit.route.a.lot.common.Coordinates;
+
+
+public class ShellGUI {
+    static GUI gui;
+    
+    public static void main(String[] args) {
+        gui = new GUI(new Coordinates());
+        gui.setBounds(0, 25, 500, 500);
+        System.out.println(gui.getWidth());
+        System.out.println(gui.getHeight());
+        gui.setVisible(true);
+        gui.addContents();
+    }
+    
+}
