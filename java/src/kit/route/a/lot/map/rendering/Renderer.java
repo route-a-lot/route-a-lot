8652f354a9a38a8437248426cd764f97d12a634c
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2012-01-17 02:33:26 +0000

    Renderer zeichnet jetzt einzelne Kacheln und cacht sie

--- java/src/kit/route/a/lot/map/rendering/Renderer.java
+++ java/src/kit/route/a/lot/map/rendering/Renderer.java
@@ -16,102 +16,87 @@ public class Renderer {
      * A cache storing tiles that were previously drawn.
      */
     private RenderCache cache;
-    
-    protected State state = State.getInstance();
 
+    protected State state = State.getInstance();
+    
     /**
      * Creates a new renderer.
      */
     public Renderer() {
-        cache = null;
+        cache = new HashRenderCache();
     }
-    
+
     /**
      * Renders a map viewing rectangle using the given rendering context.
      * 
-     * @param topLeft northwestern corner of the viewing rectangle
-     * @param bottomRight southeastern corner of the viewing rectangle
-     * @param detail level of detail of the map view
-     * @param renderingContext the output rendering context
+     * @param context
+     *            the rendering context
+     * @param detail
+     *            level of detail of the map view
      */
     public void render(Context context, int detail) {
-        // TEMPORARY: create one big tile only, don't cache
-        Tile out = new Tile(context.getTopLeft(), context.getBottomRight(), detail,
-                context.getWidth(), context.getHeight());
-        out.prerender(state);
-        context.drawImage(context.getTopLeft(), out.getData());
-        /*
-         * prerenderTiles(rc.getTopLeft(), rc.getBottomRight(), detail);
-         * ...
-         */
-    }
-
-    
-
-    /**
-     * Determines all tiles being part of the given map rectangle and pre-renders them if necessary.
-     * 
-     * @param topLeft northwestern corner of the map rectangle
-     * @param bottomRight southeastern corner of the map rectangle
-     * @param detail level of detail of the map view
-     */
-    @SuppressWarnings("unused")
-    private void prerenderTiles(Coordinates topLeft, Coordinates bottomRight, int detail) {
         float tileDim = (float) (Tile.BASE_TILE_DIM * Math.exp(detail * Math.log(2)));
-        int maxLon = (int) Math.floor(bottomRight.getLongitude() / tileDim);
-        int maxLat = (int) Math.floor(bottomRight.getLatitude() / tileDim);
-        for (int i = (int) Math.floor(topLeft.getLongitude() / tileDim); i <= maxLon; i++) {
-            for (int k = (int) Math.floor(topLeft.getLatitude() / tileDim); k <= maxLat; k++) {
-                prerenderTile(new Tile(new Coordinates(i * tileDim, k * tileDim),
-                                       new Coordinates((i+1) * tileDim, (k+1) * tileDim),
-                                       detail));
+        int maxLon = (int) Math.floor(context.getBottomRight().getLongitude() / tileDim);
+        int maxLat = (int) Math.floor(context.getTopLeft().getLatitude() / tileDim) + 1;
+        for (int i = (int) Math.floor(context.getTopLeft().getLongitude() / tileDim); i <= maxLon; i++) {
+            for (int k = (int) Math.floor(context.getBottomRight().getLatitude() / tileDim) + 1; k <= maxLat; k++) {
+                Coordinates topLeft = new Coordinates((k + 1) * tileDim, i * tileDim);
+                Coordinates bottomRight = new Coordinates(k * tileDim, (i + 1) * tileDim);
+                Tile currentTile = prerenderTile(topLeft, bottomRight, detail, context.getScale());
+                context.drawImage(topLeft, currentTile.getData());
             }
         }
     }
 
+
     /**
-     * If necessary, draws (and caches) the given tile.
+     * If necessary, renders and caches the tile with the specified data
      * 
-     * @param tile the given tile frame
+     * @return the rendered tile
      */
-    private void prerenderTile(Tile tile) {
-        if (!cache.queryCache(tile)) {
+    private Tile prerenderTile(Coordinates topLeft, Coordinates bottomRight, int detail, float scale) {
+        Tile tile = cache.queryCache(Tile.getSpecifier(topLeft, detail));
+        if (tile == null) {
+            tile = new Tile(topLeft, bottomRight, detail, scale);
             tile.prerender(state);
             cache.addToCache(tile);
         }
+        return tile;
     }
 
     /**
-     * Chooses an so far uncached tile in proximity of the visible map
-     * viewing rectangle, subsequently drawing and caching it.
+     * Chooses an so far uncached tile in proximity of the visible map viewing rectangle, subsequently drawing
+     * and caching it.
      * 
      * @return true if a tile was drawn
      */
     public boolean prerenderIdle() {
         return false; // TODO: implement
     }
-    
+
     /**
      * Draws the given route on the current rendering context.
      * 
-     * @param route node ids of the route nodes
-     * @param selection navigation point list
+     * @param route
+     *            node ids of the route nodes
+     * @param selection
+     *            navigation point list
      */
-    @SuppressWarnings("unused")
     private void drawRoute(List<Integer> route, List<Selection> selection) {
     }
-    
+
     /**
      * Draws a point of interest on the current rendering context.
      * 
-     * @param poi the POI to be drawn
+     * @param poi
+     *            the POI to be drawn
      */
-    @SuppressWarnings("unused")
     private void drawPOI(POINode poi) {
     }
 
     /**
      * Adopts the cache from another renderer.
+     * 
      * @param source
      */
     public void inheritCache(Renderer source) {
