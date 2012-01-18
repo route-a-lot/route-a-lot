4dd4b4bbb90d9d730bd50c41f69aeb80a2a7055f
Yvonne Braun <yvonne.braun@student.kit.edu>
2012-01-18 11:46:38 +0000

    SRTMLoader liest Tiles ein (ungetestet), Kommunikation mit Heightmap fehlt noch

--- java/src/kit/route/a/lot/io/HeightLoader.java
+++ java/src/kit/route/a/lot/io/HeightLoader.java
@@ -1,28 +1,12 @@
 package kit.route.a.lot.io;
 
-import kit.route.a.lot.heightinfo.HeightTile;
 import java.io.File;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.EOFException;
 
 
 
-public class HeightLoader {
 
-    int count = 0;
-    int input;
-    int width = 1201;
-    int height = 1201;
-    int lat = 0;
-    int lon = 0;
+public interface HeightLoader {
 
-    HeightTile tile = null;
-    FileInputStream in = null;
-    DataInputStream bin = null;
-
-    
     /**
      * Operation load
      * 
@@ -31,8 +15,5 @@ public class HeightLoader {
      * @return
      * @return
      */
-    public void load(File file) {
-        //einlesen der einzelnen Tiles
-        File[] dateien = file.listFiles();
-    }
+    public void load(File file);
 }
--- java/src/kit/route/a/lot/io/SRTMLoader.java
+++ java/src/kit/route/a/lot/io/SRTMLoader.java
@@ -1,13 +1,74 @@
 package kit.route.a.lot.io;
 
+import kit.route.a.lot.heightinfo.HeightTile;
+import kit.route.a.lot.common.Coordinates;
 import java.io.File;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.EOFException;
+
+
 
 
 public class SRTMLoader implements HeightLoader {
+    
+        int count = 0;
+        int input;
+        int width = 1201;
+        int height = 1201;
+        int lat = 0;
+        int lon = 0;
 
-    @Override
-    public void load(File file) {
-        // TODO Auto-generated method stub
+        HeightTile tile;
+        FileInputStream in;
+        DataInputStream bin;
+        Coordinates origin;
 
+        @Override
+        public void load(File file) {
+        
+            File[] dateien = file.listFiles();
+            
+            for(int k = 0; k < dateien.length; k++){
+                String[] arr = dateien[k].getName().split("");
+                Integer hunderter;
+                Integer zehner;
+                Integer einer;
+                /* origin auslesen */
+                for(int l = 0; l < arr.length; l++) {
+                    if(arr[l].equals("N")) {
+                        zehner = Integer.valueOf(arr[l+1]);
+                        einer = Integer.valueOf(arr[l+2]);
+                        lat = zehner.intValue()*10 + einer.intValue();
+                    } else if (arr[l].equals("E")) {
+                        hunderter = Integer.valueOf(arr[l+1]);
+                        zehner = Integer.valueOf(arr[l+2]);
+                        einer = Integer.valueOf(arr[l+3]);
+                        lon = hunderter.intValue()*100+zehner.intValue()*10+
+                                  einer.intValue();
+                    }
+                }//end for origin
+              origin = new Coordinates((float)lat, (float)lon);
+              tile = new HeightTile(width, height, origin);
+              
+              try {    
+                  in = new FileInputStream(dateien[k]);
+                  bin = new DataInputStream(in);
+
+                  for(int i = 0; i < height; i++){ 
+                      for(int j = 0; j < width; j++){
+                          tile.setHeight(i,j,bin.readShort());    
+                      }//for width
+                  }//for height
+                  bin.close();
+                  in.close();
+              } catch (EOFException eof) {
+                  System.out.println(eof);
+              } catch (IOException e) {
+                  System.out.println(e);
+              }
+          
+      }//end for dateien
     }
 }
