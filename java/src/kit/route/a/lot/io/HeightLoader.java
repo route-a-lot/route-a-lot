b680e890a56b92019a20f86a4d0f9039a19c690c
Yvonne Braun <yvonne.braun@student.kit.edu>
2012-01-18 10:41:19 +0000

    HeightLoader: imports und Variablen

--- java/src/kit/route/a/lot/io/HeightLoader.java
+++ java/src/kit/route/a/lot/io/HeightLoader.java
@@ -1,10 +1,28 @@
 package kit.route.a.lot.io;
 
+import kit.route.a.lot.heightinfo.HeightTile;
 import java.io.File;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.EOFException;
 
 
-public interface HeightLoader {
 
+public class HeightLoader {
+
+    int count = 0;
+    int input;
+    int width = 1201;
+    int height = 1201;
+    int lat = 0;
+    int lon = 0;
+
+    HeightTile tile = null;
+    FileInputStream in = null;
+    DataInputStream bin = null;
+
+    
     /**
      * Operation load
      * 
@@ -13,6 +31,8 @@ public interface HeightLoader {
      * @return
      * @return
      */
-    public void load(File file);
-
+    public void load(File file) {
+        //einlesen der einzelnen Tiles
+        File[] dateien = file.listFiles();
+    }
 }
