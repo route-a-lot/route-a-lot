469e82b555f71e9de753fe37aaed0bd185755093
Yvonne Braun <yvonne.braun@student.kit.edu>
2012-01-21 20:51:47 +0000

    TestSRTMLoader

---
+++ test/src/kit/route/a/lot/io/SRTMLoaderProto.java
@@ -0,0 +1,120 @@
+package kit.route.a.lot.io;
+
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import kit.route.a.lot.common.Coordinates;
+import kit.route.a.lot.heightinfo.HeightTile;
+import kit.route.a.lot.heightinfo.Heightmap;
+
+
+public class SRTMLoaderProto {
+    /**
+     * Operation load
+     * 
+     * @param file
+     *            -
+     * @return
+     * @return
+     **/
+    /*zum testen*/
+    private Heightmap heightmap;
+    private int width;
+    private int height;
+
+    public SRTMLoaderProto(){
+        this.width = 1201;
+        this.height = 1201;
+        this.heightmap = new Heightmap();
+    }
+    
+    //--------------laod-----------------------------------------------
+    public void load(File file) {
+        
+        File[] dateien = file.listFiles();
+        HeightTile tile;
+        FileInputStream in;
+        DataInputStream bin;
+        Coordinates origin;
+        int lat = 0;
+        int lon = 0;
+
+        for(int k = 0; k < dateien.length; k++){
+            String[] arr = dateien[k].getName().split("");
+            Integer hunderter;
+            Integer zehner;
+            Integer einer;
+            /* origin auslesen */
+            for(int l = 0; l < arr.length; l++){
+                if(arr[l].equals("N")){
+                    zehner = Integer.valueOf(arr[l+1]);
+                    einer = Integer.valueOf(arr[l+2]);
+                    lat = zehner.intValue()*10 + einer.intValue();
+                } else if (arr[l].equals("E")){
+                    hunderter = Integer.valueOf(arr[l+1]);
+                    zehner = Integer.valueOf(arr[l+2]);
+                    einer = Integer.valueOf(arr[l+3]);
+                    lon = hunderter.intValue()*100+zehner.intValue()*10+einer.intValue();
+                }
+            
+            }//end for origin
+            System.out.println("lon: " + lon);
+            System.out.println("lat: " + lat);
+            origin = new Coordinates((float)lat, (float)lon);
+            tile = new HeightTile(width, height, origin);
+            
+            try{    
+                in = new FileInputStream(dateien[k]);
+                bin = new DataInputStream(in);
+
+                for(int i = 0; i < height; i++){ 
+                    for(int j = 0; j < width; j++){
+                        tile.setHeight(i,j,bin.readShort());
+                    }//for width
+                }//for height
+                bin.close();
+                in.close();
+            } catch (EOFException eof){
+                eof.printStackTrace();
+            } catch (IOException e){
+                System.out.println(e);
+            }
+            /*in HeightMap einfügen*/
+            heightmap.addHeightTile(tile);
+            test(tile);
+        }//end for dateien
+    }//end load
+    
+    /*------------Test------------------------------------*/
+    private void test(HeightTile tile){
+        File datei = new File("test/resources/Daten/tileInhalt.txt");
+        String output = "";
+        try{
+            BufferedWriter out = new BufferedWriter(new FileWriter(datei));
+    
+            for(int i = 0; i < height; i++){
+                for(int j = 0; j < width; j++){
+                    output = output +" "+ tile.getHeight(i,j);
+                }
+                out.write(output);
+                out.newLine();
+                output = "";
+            }
+            out.close();            
+        } catch(IOException e) {
+            System.out.println("Fehler beim auslesen");
+        }
+    }//end test
+    
+    /*-----------TestHeighMap---------------------------------*/
+    public Heightmap getHeightMap(){
+        return heightmap;
+    }
+    
+    
+}
---
+++ test/src/kit/route/a/lot/io/SRTMTester.java
@@ -0,0 +1,40 @@
+package kit.route.a.lot.io;
+
+import java.io.File;
+
+import kit.route.a.lot.common.Coordinates;
+
+
+public class SRTMTester {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SRTMLoaderProto loader = new SRTMLoaderProto();
+        File datei = new File("test/resources/Daten");
+        loader.load(datei);
+        //System.out.println("test: "+test);    
+
+
+
+/*--------------------------Test1-------------------------------------------*/
+        Coordinates coordinates = new Coordinates(49.0030802f,8.3672265f);
+        System.out.println("Höhe an der Stelle: lat: "+ coordinates.getLatitude()                       + " lon: " + coordinates.getLongitude()+" : "+
+                                loader.getHeightMap().getHeight(coordinates) );
+/*----------------------------Test2------------------------------------------*/
+  coordinates = new Coordinates(49.0055222f,8.4034558f);
+        System.out.println("Höhe an der Stelle: lat: "+ coordinates.getLatitude()                       + " lon: " + coordinates.getLongitude()+" : "+
+                                loader.getHeightMap().getHeight(coordinates) );
+/*-----------------------------Test3------------------------------------------*/
+  coordinates = new Coordinates(48.9980814f,8.44314f);
+        System.out.println("Höhe an der Stelle: lat: "+ coordinates.getLatitude()                       + " lon: " + coordinates.getLongitude()+" : "+
+                                loader.getHeightMap().getHeight(coordinates) );
+
+
+
+
+
+    }
+
+}
