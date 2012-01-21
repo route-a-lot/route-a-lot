055af9d8be2c0577a0c7d854b6309248c89cd4a0
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2012-01-21 12:09:02 +0000

    HashRendererCache verbessert Jetzt wird wirklich LRU verwendet

--- java/src/kit/route/a/lot/map/rendering/HashRenderCache.java
+++ java/src/kit/route/a/lot/map/rendering/HashRenderCache.java
@@ -20,30 +20,34 @@ public class HashRenderCache implements RenderCache {
     /**
      * a FIFO list for the cache replacement
      */
-    private LinkedList<Tile> leastRecentlyUsed; // TODO EXTEND: more elaborate aging algorithm
+    private LinkedList<Long> leastRecentlyUsed; // TODO EXTEND: more elaborate aging algorithm
 
     public HashRenderCache() {
         resetCache();
     }
-    
+
     @Override
     public Tile queryCache(long tileSpecifier) {
+        if (leastRecentlyUsed.remove(tileSpecifier)) {
+            leastRecentlyUsed.addLast(tileSpecifier);
+        }
         return map.get(tileSpecifier);
     }
     
     @Override
     public void addToCache(Tile tile) {
-        map.put(tile.getSpecifier(), tile);
-        if (leastRecentlyUsed.size() >= CACHE_SIZE) {
-            map.remove(leastRecentlyUsed.removeFirst().hashCode());   
+        long specifier = tile.getSpecifier();
+        map.put(specifier, tile);
+        if (leastRecentlyUsed.size() > CACHE_SIZE) {
+            map.remove(leastRecentlyUsed.removeFirst());   
         }
-        leastRecentlyUsed.addLast(tile);
+        leastRecentlyUsed.addLast(specifier);
     }
 
     @Override
     public void resetCache() {
         map = new HashMap<Long, Tile>();
-        leastRecentlyUsed = new LinkedList<Tile>();
+        leastRecentlyUsed = new LinkedList<Long>();
     }
     
 }
