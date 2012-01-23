0a108a8603a84a97a9dced3f93d8ba6e82be91fb
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2012-01-23 17:39:36 +0000

    ungetestete Version des KML-Exports

--- java/src/kit/route/a/lot/io/RouteIO.java
+++ java/src/kit/route/a/lot/io/RouteIO.java
@@ -1,6 +1,33 @@
 package kit.route.a.lot.io;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+
+import javax.print.Doc;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import kit.route.a.lot.common.Coordinates;
+import kit.route.a.lot.common.Selection;
+import kit.route.a.lot.controller.State;
+import kit.route.a.lot.map.infosupply.MapInfo;
+import kit.route.a.lot.map.rendering.Projection;
 
 
 public class RouteIO {
@@ -36,5 +63,115 @@ public class RouteIO {
      * @return
      */
     public static void exportCurrentRouteToKML(File file) {
+        try {
+            State state = State.getInstance();
+            List<Integer> route = state.getCurrentRoute();
+            List<Selection> navNodes = state.getNavigationNodes();
+
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+
+            // TODO test here
+            document.createProcessingInstruction("version", "1.0");
+            document.createProcessingInstruction("encoding", "UTF-8");
+
+            Element kmlElement = document.createElement("kml");
+            document.appendChild(kmlElement);
+            kmlElement.setAttribute("xmlns", "http://earth.google.com/kml/2.1");
+            Element documentElement = document.createElement("Document");
+            kmlElement.appendChild(documentElement);
+            
+            Element styleElement = document.createElement("Style");
+            documentElement.appendChild(styleElement);
+            styleElement.setAttribute("id", "roadStyle");
+            Element lineStyleElement = document.createElement("LineStyle");
+            styleElement.appendChild(lineStyleElement);
+            Element colorElement = document.createElement("color");
+            lineStyleElement.appendChild(colorElement);
+            colorElement.appendChild(document.createTextNode("ffffff00"));
+            Element widthElement = document.createElement("width");
+            lineStyleElement.appendChild(widthElement);
+            widthElement.appendChild(document.createTextNode("6"));
+            
+            Element openElement = document.createElement("Open");
+            documentElement.appendChild(openElement);
+            openElement.appendChild(document.createTextNode("1"));
+            Element descriptionElement = document.createElement("description");
+            documentElement.appendChild(descriptionElement);
+            descriptionElement.appendChild(document.createTextNode("Route calculated by 'Route A Lot'"));
+            Element nameElement = document.createElement("name");
+            documentElement.appendChild(nameElement);
+            nameElement.appendChild(document.createTextNode("Route"));
+            Element placemarkElement = document.createElement("Placemark");
+            documentElement.appendChild(placemarkElement);
+            nameElement = document.createElement("name");
+            placemarkElement.appendChild(nameElement);
+            Element styleUrlElement = document.createElement("styleUrl");
+            placemarkElement.appendChild(styleUrlElement);
+            styleUrlElement.appendChild(document.createTextNode("#roadStyle"));
+            Element multiGeometryElement = document.createElement("MultiGeometry");
+            placemarkElement.appendChild(multiGeometryElement);
+            Element lineStringElement = document.createElement("LineString");
+            multiGeometryElement.appendChild(lineStringElement);
+            Element coordinatesElement = document.createElement("coordinates");
+            lineStringElement.appendChild(coordinatesElement);
+            
+            StringBuilder coordinatesSB = new StringBuilder();
+            Projection projection = Projection.getProjectionForCurrentMap();
+            MapInfo mapInfo = state.getLoadedMapInfo();
+            for (int i = 0; i < route.size(); i++) {
+                Coordinates localCoordinates = mapInfo.getNode(route.get(i)).getPos();
+                Coordinates geoCoordinates = projection.geoCoordinatesToLocalCoordinates(localCoordinates);
+                
+                coordinatesSB.append(geoCoordinates.getLongitude() + "," + geoCoordinates.getLatitude());
+                if (i % 3 == 2) {
+                    coordinatesSB.append("\n");
+                } else {
+                    coordinatesSB.append(" ");
+                }
+            }
+            coordinatesElement.appendChild(document.createTextNode(coordinatesSB.toString()));
+            
+            Element folderElement = document.createElement("Folder");
+            documentElement.appendChild(folderElement);
+            nameElement = document.createElement("name");
+            folderElement.appendChild(nameElement);
+            nameElement.appendChild(document.createTextNode("waypoints"));
+            for (int i = 0; i < navNodes.size(); i++) {
+                placemarkElement = document.createElement("Placemark");
+                folderElement.appendChild(placemarkElement);
+                nameElement = document.createElement("name");
+                placemarkElement.appendChild(nameElement);
+                String name;
+                if (i == 0) {   // TODO i18n
+                    name = "Start";
+                } else if (i == navNodes.size() - 1) {
+                    name = "Ziel";
+                } else {
+                    name = "Zwischenhalt";
+                }
+                nameElement.appendChild(document.createTextNode(name));
+                Element pointElement = document.createElement("Point");
+                placemarkElement.appendChild(pointElement);
+                coordinatesElement = document.createElement("coordinates");
+                pointElement.appendChild(coordinatesElement);
+                Coordinates nodePosition = navNodes.get(i).getPosition();
+                String coordinates = nodePosition.getLongitude() + "," + nodePosition.getLatitude();
+                pointElement.appendChild(document.createTextNode(coordinates));
+            }
+            
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            StreamResult result = new StreamResult(new StringWriter());
+            DOMSource source = new DOMSource(document);
+            transformer.transform(source, result);
+            String xmlString = result.getWriter().toString();
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(xmlString);
+            writer.flush();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
