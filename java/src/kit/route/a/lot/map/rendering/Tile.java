64e880e3377f7ad4d9ee28a8a07cec700e376fbc
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2012-01-25 22:13:30 +0000

    Straßen werden je nach Typ unterschiedlich dick und 'runder' gezeichnet Bei Einbahnstraßen geben Pfeile deren Richtung an

--- java/src/kit/route/a/lot/map/rendering/Tile.java
+++ java/src/kit/route/a/lot/map/rendering/Tile.java
@@ -4,6 +4,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.awt.image.BufferedImage;
 import java.util.Collection;
 
@@ -116,6 +117,12 @@ public class Tile {
         for (MapElement element : map) {
             if (element instanceof Street) {
                 draw((Street) element, true, graphics);
+            }
+        }
+        
+        for (MapElement element : map) {
+            if (element instanceof Street) {
+                drawStreetArrows((Street) element, graphics);
             }
         }
 
@@ -243,25 +250,20 @@ public class Tile {
      *            the street to be drawn
      */
     protected void draw(Street street, boolean top, Graphics2D graphics) {
-        int[] xPoints, yPoints;
-        int nPoints;
         Node[] nodes = street.getNodes();
-        nPoints = nodes.length;
-        xPoints = new int[nPoints];
-        yPoints = new int[nPoints];
+        int nPoints = nodes.length;
+        int[] xPoints = new int[nPoints];
+        int[] yPoints = new int[nPoints];
 
-        for (int i = 0; i < nPoints; i++) {
-            Coordinates curCoordinates = Renderer.getLocalCoordinatesFromGlobalCoordinates(nodes[i].getPos(), topLeft, detail);
-            xPoints[i] = (int) curCoordinates.getLongitude();
-            yPoints[i] = (int) curCoordinates.getLatitude();
-        }
+        WayInfo wayInfo = street.getWayInfo();
+        int basicSize = 10;
 
-        if (!top) {
-            graphics.setStroke(new BasicStroke(5));
-            graphics.setColor(Color.DARK_GRAY);
-            graphics.drawPolyline(xPoints, yPoints, nPoints);
-        } else {
-            WayInfo wayInfo = street.getWayInfo();
+        // set colors
+        if (top) {
+            graphics.setColor(Color.WHITE);
+            if (!wayInfo.isRoutable()) {
+                graphics.setColor(Color.LIGHT_GRAY);
+            }
             switch (wayInfo.getType()) {
                 case OSMType.HIGHWAY_MOTORWAY:
                 case OSMType.HIGHWAY_MOTORWAY_JUNCTION:
@@ -272,14 +274,128 @@ public class Tile {
                 case OSMType.HIGHWAY_PRIMARY_LINK:
                     graphics.setColor(new Color(255, 204, 51));
                     break;
-                default:
-                    graphics.setColor(Color.WHITE);
             }
-
-            graphics.setStroke(new BasicStroke(3));
-            graphics.drawPolyline(xPoints, yPoints, nPoints);
+        } else {
+            graphics.setColor(Color.DARK_GRAY);
         }
 
+        // set size
+        switch (wayInfo.getType()) {
+            case OSMType.HIGHWAY_MOTORWAY:
+            case OSMType.HIGHWAY_MOTORWAY_JUNCTION:
+            case OSMType.HIGHWAY_MOTORWAY_LINK:
+                basicSize = 40;
+                break;
+            case OSMType.HIGHWAY_PRIMARY:
+            case OSMType.HIGHWAY_PRIMARY_LINK:
+                basicSize = 30;
+                break;
+            case OSMType.HIGHWAY_SECONDARY:
+            case OSMType.HIGHWAY_SECONDARY_LINK:
+                basicSize = 22;
+                break;
+            case OSMType.HIGHWAY_TERTIARY:
+            case OSMType.HIGHWAY_TERTIARY_LINK:
+            case OSMType.HIGHWAY_RESIDENTIAL:
+            case OSMType.HIGHWAY_LIVING_STREET:
+                basicSize = 18;
+            case OSMType.HIGHWAY_CYCLEWAY:
+                basicSize = 15;
+                break;
+        }
+        int size = basicSize / Projection.getZoomFactor(detail);
+        if (!top) {
+            graphics.setStroke(new BasicStroke(size + 2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        } else {
+            graphics.setStroke(new BasicStroke(size, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        }
+
+        for (int i = 0; i < nPoints; i++) {
+            Coordinates curCoordinates = getLocalCoordinatesFromGlobalCoordinates(nodes[i].getPos());
+            xPoints[i] = (int) curCoordinates.getLongitude();
+            yPoints[i] = (int) curCoordinates.getLatitude();
+        }
+        
+        graphics.drawPolyline(xPoints, yPoints, nPoints);
+        
+        
+    }
+    
+    protected void drawStreetArrows(Street street, Graphics2D graphics) {
+        if (detail > 1 || street.getWayInfo().getOneway() == WayInfo.ONEWAY_NO) {
+            return;
+        }
+        
+        Node[] nodes = street.getNodes();
+        int nPoints = nodes.length;
+        
+        float arrowLength = 12.f / Projection.getZoomFactor(detail);
+        float headLength = arrowLength / 2;
+        double arrowDistance = 12 * arrowLength;
+        double currentDistance = 0;
+
+        graphics.setColor(new Color(217, 192, 129));
+        graphics.setStroke(new BasicStroke(2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
+
+        for (int i = 1; i < nPoints; i++) {
+            Coordinates from = getLocalCoordinatesFromGlobalCoordinates(nodes[i-1].getPos());
+            Coordinates to = getLocalCoordinatesFromGlobalCoordinates(nodes[i].getPos());
+            currentDistance += Street.getDistanceProj(from, to);
+
+            if (currentDistance > arrowDistance) {
+                currentDistance = 0;
+
+                Coordinates vector = new Coordinates();
+                vector.setLatitude((to.getLatitude() - from.getLatitude()));
+                vector.setLongitude((to.getLongitude() - from.getLongitude()));
+                Coordinates edgeMiddle = new Coordinates();
+                edgeMiddle.setLatitude(from.getLatitude() + vector.getLatitude() / 2);
+                edgeMiddle.setLongitude(from.getLongitude() + vector.getLongitude() / 2);
+                vector = normalizeVector(vector);
+
+                Coordinates arrowStart = new Coordinates();
+                arrowStart.setLatitude(edgeMiddle.getLatitude() - vector.getLatitude() * arrowLength);
+                arrowStart.setLongitude(edgeMiddle.getLongitude() - vector.getLongitude() * arrowLength);
+                Coordinates arrowEnd = new Coordinates();
+                arrowEnd.setLatitude(edgeMiddle.getLatitude() + vector.getLatitude() * arrowLength);
+                arrowEnd.setLongitude(edgeMiddle.getLongitude() + vector.getLongitude() * arrowLength);
+
+                Coordinates headLeft = new Coordinates();
+                Coordinates rotatedVector = normalizeVector(rotateVector(vector, 210));
+                headLeft.setLatitude(arrowEnd.getLatitude() + rotatedVector.getLatitude() * headLength);
+                headLeft.setLongitude(arrowEnd.getLongitude() + rotatedVector.getLongitude() * headLength);
+                Coordinates headRight = new Coordinates();
+                rotatedVector = normalizeVector(rotateVector(vector, 150));
+                headRight.setLatitude(arrowEnd.getLatitude() + rotatedVector.getLatitude() * headLength);
+                headRight.setLongitude(arrowEnd.getLongitude() + rotatedVector.getLongitude() * headLength);
+
+                graphics.drawLine((int) arrowStart.getLongitude(), (int) arrowStart.getLatitude(), (int) arrowEnd.getLongitude(), (int) arrowEnd.getLatitude());
+                if (street.getWayInfo().getOneway() == WayInfo.ONEWAY_OPPOSITE) {
+                    arrowEnd = arrowStart;
+                }
+                graphics.drawLine((int) arrowEnd.getLongitude(), (int) arrowEnd.getLatitude(), (int) headLeft.getLongitude(), (int) headLeft.getLatitude());
+                graphics.drawLine((int) arrowEnd.getLongitude(), (int) arrowEnd.getLatitude(), (int) headRight.getLongitude(), (int) headRight.getLatitude());
+            }
+        }
+    }
+    
+    private Coordinates getLocalCoordinatesFromGlobalCoordinates(Coordinates coordinates) {
+        return Renderer.getLocalCoordinatesFromGlobalCoordinates(coordinates, topLeft, detail);
+    }
+    
+    private Coordinates normalizeVector(Coordinates vector) {
+        Coordinates newVector = new Coordinates();
+        float vectorLength = (float) Math.sqrt(Math.pow(vector.getLatitude(), 2) + Math.pow(vector.getLongitude(), 2));
+        newVector.setLatitude(vector.getLatitude() / vectorLength);
+        newVector.setLongitude(vector.getLongitude() / vectorLength);
+        return newVector;
+    }
+    
+    private Coordinates rotateVector(Coordinates vector, int angle) {
+        Coordinates rotatedVector = new Coordinates();
+        rotatedVector.setLongitude((float) ((Math.cos(angle * Math.PI / 180) * vector.getLongitude() - Math.sin(angle * Math.PI / 180) * vector.getLatitude()) * 180 / Math.PI));
+        rotatedVector.setLatitude((float) ((Math.sin(angle * Math.PI / 180) * vector.getLongitude() + Math.cos(angle * Math.PI / 180) * vector.getLatitude()) * 180 / Math.PI));
+        return rotatedVector;
     }
 
     /**
