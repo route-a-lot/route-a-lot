b25afb1101689fb0802abedabd05f3842d47d727
Yvonne Braun <yvonne.braun@student.kit.edu>
2012-01-26 20:55:24 +0000

    neuer StringTree, sortierte ArrayList(RadixSort)

--- java/src/kit/route/a/lot/common/StringTrie.java
+++ java/src/kit/route/a/lot/common/StringTrie.java
@@ -4,14 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import kit.route.a.lot.map.MapElement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.lang.Character;
+import java.lang.String;
+import java.util.Arrays;
+import java.util.Collection;
 
 
-public class StringTrie<T> {
+public class StringTrie {
     
     private ArrayList<MapElement> tree;
+    private Iterator<MapElement> iterator;
+    private int maxLength;
     
     public StringTrie(){
-        tree = new ArrayList();
+        tree = new ArrayList<MapElement>();
+        this.maxLength = 0;
     }
     
     
@@ -21,6 +30,9 @@ public class StringTrie<T> {
     
     public void insert(String name, MapElement element) {
                 tree.add(element);
+                if(element.getName().length() > maxLength){
+                    maxLength = element.getName().length();
+                }
     }
 
     /**
@@ -30,8 +42,49 @@ public class StringTrie<T> {
      *            -
      * @return List<T>
      */
-    public List<T> search(String name) {
+    public List<String> search(String name) {
         /*Textvervollständigung ist nicht implementiert*/
         return null;
     }
+    
+    public void RadixSort(){
+        for(int i = (maxLength-1); i >=0; i--){
+            iterator = tree.iterator();
+            while(iterator.hasNext()){
+                MapElement e = iterator.next();
+                if(e.getName().length() < (i+1)){
+                    e.setID(0);
+                } else {
+
+                    e.setID((Character.getNumericValue(e.getName().charAt(i))) - 10);
+                }//end if
+            }//end while
+                System.out.println("ksort");
+                kSort();
+        }//end for
+
+    }//radixsort
+    
+    public void kSort(){
+        ArrayList<MapElement> [] buckets = new ArrayList[26];
+        for(int i =0; i<26; i++){
+            buckets[i] = new ArrayList<MapElement>();
+        }//end for
+        iterator =  tree.iterator();
+        while(iterator.hasNext()){
+            MapElement element = iterator.next();
+            System.out.println(element.getID());
+            ArrayList<MapElement> list = buckets[element.getID()];
+            buckets[element.getID()].add(element);
+        }//end while
+        tree = buckets[0];
+        for(int i = 1; i < 26; i++){
+            tree.addAll(buckets[i]);
+            if(buckets[i] == null){
+                System.out.println("bucket ist null");
+            };
+        }//end for
+
+    }//end ksort
+
 }
