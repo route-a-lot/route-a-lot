79c567cbf3a2d8e9d1ece68e1915897505f0ad0f
Yvonne Braun <yvonne.braun@student.kit.edu>
2012-01-27 12:18:54 +0000

    StringTrie mit SuggestCompletions

--- java/src/kit/route/a/lot/common/StringTrie.java
+++ java/src/kit/route/a/lot/common/StringTrie.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import kit.route.a.lot.map.MapElement;
+import kit.route.a.lot.map.Street;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.lang.Character;
@@ -33,21 +35,28 @@ public class StringTrie {
                 if(element.getName().length() > maxLength){
                     maxLength = element.getName().length();
                 }
-    }
+    }   
 
-    /**
-     * Operation search
-     * 
-     * @param name
-     *            -
-     * @return List<T>
-     */
-    public List<String> search(String name) {
-        /*Textvervollständigung ist nicht implementiert*/
-        return null;
+    public ArrayList<String> search(String name) {
+        int first = Character.getNumericValue(name.charAt(0));
+        String tmpName;
+        ArrayList<String> completions = new ArrayList<String>(10);
+        Street [] tmp = new Street[1];
+        Street[] elements = tree.toArray(tmp);
+        for(int i = 0; i < elements.length;i++){
+            tmpName = elements[i].getName();
+            if(first < Character.getNumericValue(tmpName.charAt(0))){
+                /*bricht ab wenn erster Buchstabe lexikographisch größer ist*/
+                return completions;
+            }else if(tmpName.startsWith(name)){
+                completions.add(elements[i].getName());
+            }
+        }
+        return completions;
     }
     
-    public void RadixSort(){
+    
+    public void radixSort(){
         for(int i = (maxLength-1); i >=0; i--){
             iterator = tree.iterator();
             while(iterator.hasNext()){
@@ -55,36 +64,38 @@ public class StringTrie {
                 if(e.getName().length() < (i+1)){
                     e.setID(0);
                 } else {
-
-                    e.setID((Character.getNumericValue(e.getName().charAt(i))) - 10);
-                }//end if
-            }//end while
+                    int id = Character.getNumericValue(e.getName().charAt(i)) - 10;
+                    if((id > 25)|| (id < 0)){id = 25;} /*Sonderzeichen*/
+                    e.setID(id);
+                }
+            }
                 System.out.println("ksort");
                 kSort();
-        }//end for
+        }
 
     }//radixsort
+   
     
     public void kSort(){
         ArrayList<MapElement> [] buckets = new ArrayList[26];
         for(int i =0; i<26; i++){
             buckets[i] = new ArrayList<MapElement>();
-        }//end for
+        }
         iterator =  tree.iterator();
         while(iterator.hasNext()){
             MapElement element = iterator.next();
             System.out.println(element.getID());
             ArrayList<MapElement> list = buckets[element.getID()];
             buckets[element.getID()].add(element);
-        }//end while
+        }
         tree = buckets[0];
         for(int i = 1; i < 26; i++){
             tree.addAll(buckets[i]);
             if(buckets[i] == null){
                 System.out.println("bucket ist null");
             };
-        }//end for
+        }
 
-    }//end ksort
+    }//end ksort 
 
 }
