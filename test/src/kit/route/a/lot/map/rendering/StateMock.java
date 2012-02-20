d843f71f2210bdefd8c01df1c432d52f531a4cf8
Yvonne Braun <yvonne.braun@student.kit.edu>
2012-02-20 10:10:47 +0000

    erweitert um NavigatioNodes und CurrentRoute

--- test/src/kit/route/a/lot/map/rendering/StateMock.java
+++ test/src/kit/route/a/lot/map/rendering/StateMock.java
@@ -1,15 +1,27 @@
 package kit.route.a.lot.map.rendering;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kit.route.a.lot.common.Selection;
 import kit.route.a.lot.controller.State;
-import kit.route.a.lot.map.infosupply.MapInfo;
 
 
 public class StateMock extends State {
     
     private MapInfoMock mapInfo = new MapInfoMock();
+    private List<Selection> navigationNodes = new ArrayList<Selection>();
     
     public MapInfoMock getLoadedMapInfo() {
         return mapInfo;
+    }
+    
+    public List<Selection> getNavigationNodes() {
+        return navigationNodes;
+    }
+
+    public List<Integer> getCurrentRoute() {
+        return null;
     }
 
 }
