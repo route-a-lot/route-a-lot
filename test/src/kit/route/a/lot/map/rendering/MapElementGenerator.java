0b41faceef736b0c53e2a5720ae8159cb54f67e4
Yvonne Braun <yvonne.braun@student.kit.edu>
2012-02-21 23:06:54 +0000

    Zufallsgenerator für Street

--- test/src/kit/route/a/lot/map/rendering/MapElementGenerator.java
+++ test/src/kit/route/a/lot/map/rendering/MapElementGenerator.java
@@ -17,12 +17,32 @@ public class MapElementGenerator {
     }
     
     public Street generateStreet(){
-        //TODO
-       return null; 
+        wayinfo = new WayInfo();
+        double choose = Math.random() * 2;
+        if(choose < 0.15){
+            wayinfo.setStreet(false);
+        } else {
+            wayinfo.setStreet(true);
+            wayinfo.setBicycle(WayInfo.BICYCLE_YES);
+        }
+        choose = Math.random() * 5;
+        if(choose <= 1){
+            wayinfo.setType(OSMType.HIGHWAY_RESIDENTIAL);
+        } else if(1 < choose && choose <= 2){
+            wayinfo.setType(OSMType.HIGHWAY_TERTIARY);
+        } else if(2 < choose && choose <= 3){
+            wayinfo.setType(OSMType.HIGHWAY_SECONDARY);
+        } else if(3 < choose && choose <= 4){
+            wayinfo.setType(OSMType.HIGHWAY_PRIMARY);   
+        } else if(4 < choose && choose <= 5){
+            wayinfo.setType(OSMType.HIGHWAY_MOTORWAY);   
+        }
+        Street street = new Street(null, wayinfo);
+        return street; 
     }
     
     public Area generateArea(){
-    
+        wayinfo = new WayInfo();
         double choose = Math.random() * 2;
         if(choose > 1){
             wayinfo.setBuilding(true);
