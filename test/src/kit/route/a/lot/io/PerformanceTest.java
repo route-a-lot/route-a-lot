4878fe9230dfe464a65d6daa7554283db5eaf763
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2012-02-26 11:55:57 +0000

    Methode zur Ausgabe von Informationen über den Speicherverbrauch hinzugefügt

--- test/src/kit/route/a/lot/io/PerformanceTest.java
+++ test/src/kit/route/a/lot/io/PerformanceTest.java
@@ -1,18 +1,19 @@
 package kit.route.a.lot.io;
 
+import static kit.route.a.lot.common.Util.printMemoryInformation;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
-import org.apache.log4j.PropertyConfigurator;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import kit.route.a.lot.common.Address;
 import kit.route.a.lot.common.Coordinates;
 import kit.route.a.lot.common.WayInfo;
 import kit.route.a.lot.map.infosupply.MapInfo;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class PerformanceTest {
     
@@ -28,28 +29,31 @@ public class PerformanceTest {
         mapInfo = new MapInfo();
     }
     
-    //@Test
+//    @Test
     public void testMemoryPerformanceWithNodes() {
+        printMemoryInformation();
         mapInfo.setBounds(new Coordinates(100, 10), new Coordinates(10, 100));
         Random rnd = new Random();
         for (int i = 0; i < 33000000; i++) {
             mapInfo.addNode(new Coordinates(10 + rnd.nextFloat()*90, 10 + rnd.nextFloat()*90), i, new Address());
-            if (i > 297800 && i % 10 == 0) { 
+            if (i > 25500000 && i % 1000 == 0) { 
                 System.gc();
-                // System.out.println("current node = " + i);
+                System.out.println("current node = " + i);
+                printMemoryInformation();
             }
-            if (i % 1000 == 0) {
-                // System.out.println("current node = " + i);
+            if (i % 300000 == 0) {
+                 System.out.println("current node = " + i);
+                 printMemoryInformation();
             }
         }
-        // System.out.println("Fertig");
+         System.out.println("Fertig");
     }
     
 //    @Test
     public void testMemoryPerformanceWithWays() {
         mapInfo.setBounds(new Coordinates(100, 10), new Coordinates(10, 100));
         Random rnd = new Random();
-
+        
         List<Integer> ids;
         WayInfo wayInfo;
         int type = 0;
@@ -103,4 +107,5 @@ public class PerformanceTest {
             mapInfo.printQuadTree();
         }
     }
+    
 }
