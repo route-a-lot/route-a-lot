e849d2d5251e44106180284a2bb71a8fb13dd468
Malte Wolff <maltewolff@web.de>
2012-02-28 23:57:20 +0000

    mapinfo fertig getestet

--- test/src/kit/route/a/lot/map/MapInfoTest.java
+++ test/src/kit/route/a/lot/map/MapInfoTest.java
@@ -89,5 +89,20 @@ public class MapInfoTest {
         info.addPOI(new Coordinates(7.0f, 6.7f), 12, new POIDescription("nirvana", 0, "ja das gibt es wirklich"), new Address());
         info.printQuadTree();
         assertTrue(info.getPOIDescription(new Coordinates(7.0f, 6.7f), 1.0f, 0).getName().equals("nirvana"));
+        assertTrue(info.select(new Coordinates(2.1f, 2.1f)).getName().equals("Hubert Straße"));
+        assertTrue(info.getMapElement(1).getName().equals("Fußballplatz"));
+        assertTrue(info.getNodePosition(1).getLatitude() == 10.0f);
+        info.swapNodeIds(0, 1);
+        assertTrue(info.getNodePosition(1).getLatitude() == 2.0f);
+        assertTrue(info.getBaseLayerForPositionAndRadius(new Coordinates(1.0f, 1.0f), 0.5f, true).size() == 2);
+        info.setGeoTopLeft(new Coordinates(0.1f,0.0f));
+        info.setGeoBottomRight(new Coordinates(50.0f, 75.0f));
+        assertTrue(info.getGeoBottomRight().getLongitude() == 75.0f);
+        assertTrue(info.getGeoTopLeft().getLatitude() == 0.1f);
+        info.setBounds(new Coordinates(0.0f, 1.0f), new Coordinates(12.3f, 45.6f));
+        Coordinates coord1 = new Coordinates(0.0f, 1.0f);
+        Coordinates coord2 = new Coordinates(12.3f, 45.6f);
+        info.getBounds(coord1, coord2);
+        assertTrue(coord1.getLongitude() == 1.0f);
     }
 }
